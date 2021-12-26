15ae3ded6b75d35dae31a620fd09c4ea1c62572c
Itz-NuAr <96548931+Itz-NuAr@users.noreply.github.com>
2021-12-26 14:24:28 +0200

    Delete Doroshenko_Artem_dz_2 directory

--- Doroshenko_Artem_dz_2/4.py
+++
@@ -1,6 +0,0 @@
-data = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for i in range(len(data)):
-    info = data[i]
-    list_info = info.split()
-    name = list_info[-1].capitalize()
-    print(f'Привет, {name}!')
